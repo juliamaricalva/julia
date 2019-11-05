ca1c995ce884567ce30d79a4d8325f2f7bf35759
juliamaricalva <julia.maricalva.moreno@alumnos.upm.es>
2019-11-06 00:53:05 +0800

    Add files via upload

--- hw2.sh
+++ hw2.sh
@@ -1,5 +1,3 @@
-wget https://www.dropbox.com/s/6517561r9n7fat7/model_best.pth.tar?dl=1
+wget 'https://www.dropbox.com/s/6517561r9n7fat7/model_best.pth.tar?dl=1'
 mv model_best.pth.tar?dl=1 ./log/model_best.pth.tar
-python test.py  --data_dir $1 --save_data_dir $2
-
-
+python test.py --data_dir $1 --save_data_dir $2
--- hw2_best.sh
+++ hw2_best.sh
@@ -1,4 +1,3 @@
-wget https://www.dropbox.com/s/0ykbt6cxy932qba/model_bestimproved.pth.tar?dl=1
+wget 'https://www.dropbox.com/s/0ykbt6cxy932qba/model_bestimproved.pth.tar?dl=1'
 mv model_bestimproved.pth.tar?dl=1 ./log/model_bestimproved.pth.tar
-python improved_test.py --resume --data_dir $1 --save_data_dir $2
-
+python improved_test.py --data_dir $1 --save_data_dir $2
